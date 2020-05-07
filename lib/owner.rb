063b66893c62e81d5a6f27e60b1fb2c3238822b3
Martin Chaplin <martin.chaplin44@gmail.com>
2020-05-07 18:40:15 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,25 @@
 class Owner
-  # code goes here
-end
+  attr_reader :name, :species
+  @@all= []
+  def initialize(name)
+   @species = "human"
+   @name = name 
+   @@all << self
+  end
+  def say_species
+    return "I am a human."
+  end
+  def self.all 
+    @@all 
+  end
+  def self.count
+    self.all.count
+  end
+  def self.reset_all
+    @@all.clear
+  end
+  def cats
+    Cat.name.all
+    
+  end
+ end
